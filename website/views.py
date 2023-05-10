320f71d21fa839e38cf1fb549b5be0940865de03
jakob <jakobnonnenmacher@hotmail.com>
2023-05-10 21:23:25 +0200

    totaler pfad funktioniert

--- website/views.py
+++ website/views.py
@@ -6,9 +6,11 @@ from PIL.ExifTags import TAGS
 # Create your views here.
 def index(request):
     bild = Bilder.objects.get(area="header2")
-    
+    image = Image.open("C:/Python/jachub/firmaxy/images/images/20230509_191744.jpg")
+    breite = image.height
     context = {
         "bild" : bild,
+        "breite" : breite,
     }
     return render(request, 'index.html', context)
 def about(request):
