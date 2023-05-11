8dd25076ceb284c8a6bd3c6ec8b93b7a4c4009d6
jachub <jakobnonnenmacher@hotmail.com>
2023-05-11 15:42:49 +0200

    static paths working sql selects not

--- firmaxy/settings.py
+++ firmaxy/settings.py
@@ -117,9 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 MEDIA_ROOT = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "images")
+STATIC_ROOT = "images/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- website/views.py
+++ website/views.py
@@ -2,18 +2,35 @@ from django.shortcuts import render
 from .models import *
 from PIL import Image
 from PIL.ExifTags import TAGS
+from django.conf import settings
+from django.db import connection
 
 # Create your views here.
 def index(request):
     bild = Bilder.objects.get(area="header2")
-    image = Image.open("C:/Python/jachub/firmaxy/images/images/20230509_191744.jpg")
-    breite = image.height
+    image = Image.open(bild.image)
+    height = image.height
+    height = str(height)
+
+    c = Bilder.objects.all()
+    b = Bilder.objects.filter(width = "200")
+    print(connection.queries)
+    cssFile = open("./website/static/website/bild.css", "w")
+    cssFile.write("header{height:"+height+"px;}")
     context = {
-        "bild" : bild,
-        "breite" : breite,
+        "bild" : bild.image,
+        "height" : height,
+        "b" : b,
     }
     return render(request, 'index.html', context)
 def about(request):
     return render(request, 'about.html', {})
 def galerie(request):
-    return render(request, 'galerie.html', {})
+
+    bild = Bilder.objects.get(area="galerie")
+
+    context = {
+        "bild": bild.image,
+    }
+
+    return render(request, 'galerie.html', context)
