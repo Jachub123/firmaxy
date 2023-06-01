b8875ed5ed1bd3de473b80c553e2c151c58271a7
jachub <jakobnonnenmacher@hotmail.com>
2023-06-01 15:41:48 +0200

    layout für mobile und tablet in galerie festgelegt. Effekte in Galerie dafür  angepasst.

--- website/static/website/evita.js
+++ website/static/website/evita.js
@@ -1,44 +1,91 @@
 galerie = document.querySelector(".galerieContainer")
 bilder = document.querySelectorAll(".imgContainer")
+document.addEventListener("DOMContentLoaded", () => {
+    const viewport = window.innerWidth
+    if(viewport < 992){
+        return;
+    }
 
 for(let i = 0;i < bilder.length;i++){
     bilder[i].addEventListener("mouseenter", (e) => {
-        let leftSib = e.target.previousElementSibling
-        let rightSib = e.target.nextElementSibling
         if(e.target.classList.value.includes("imgContainer")){
-            if(rightSib !== null && leftSib === null){            
-                e.target.classList.add("thick")                
-                rightSib.classList.add("thin")             
-                rightSib.nextElementSibling.classList.add("thin")             
-            }       
-            else if(rightSib === null && leftSib !== null){
-                if(e.target.offsetTop !== leftSib.offsetTop){
-                    e.target.classList.add("thick")   
-                } 
-                if(e.target.offsetTop !== rightSib.offsetTop && e.target.offsetTop === leftSib.offsetTop){
+            let leftSib = e.target.previousElementSibling
+            let rightSib = e.target.nextElementSibling
+            //EIN ITEM PRO REIHE
+            //Sind die Siblings beide nicht auf der selben Höhe wie target
+            try {
+                leftSib.offsetTop            
+            } catch (error) {                
+                //ERSTES GALERIE-BILD
+                //Wenn rechts einer ist und links keiner 
+                if(e.target.offsetTop != rightSib.offsetTop){
+                    console.log("DIESA HAT EIN ITEM PRO REIHE")
+                    e.target.classList.add("thick") 
+                }else{
+                        
+                    console.log("ERSTER BILD IN GALERIE")      
+                    e.target.classList.add("thick")                
+                    rightSib.classList.add("thin")       
+                    if(e.target.offsetTop == rightSib.nextElementSibling){      
+                        rightSib.nextElementSibling.classList.add("thin")    
+                    }         
+                    
+                }
+            }try {
+                rightSib.offsetTop
+            } catch (error) {
+                //LETZTES GALERIE-BILD
+                //Wenn rechts keiner ist und links einer   
+                if(e.target.offsetTop != leftSib.offsetTop){
+                    console.log("HUNNA EIN ITEM PRO REIHE")
+                    e.target.classList.add("thick") 
+                }
+                 else{
                     e.target.classList.add("thick")                
                     leftSib.classList.add("thin")
-                    leftSib.previousElementSibling.classList.add("thin")
-                }                            
-            }       
-            else if(rightSib !== null && leftSib !== null){
+                    if(e.target.offsetTop == leftSib.previousElementSibling){  
+                        leftSib.previousElementSibling.classList.add("thin")
+                    }
+                } 
+            }
+            try{                
+                if(e.target.offsetTop != leftSib.offsetTop && e.target.offsetTop != rightSib.offsetTop){
+                    //EIN ITEM PRO REIHE
+                    e.target.classList.add("thick")
+                }  
+            }catch (error) {}  
+            
+             
+
+ 
+   
+            //NICHT ERSTES OD. LETZTES GALERIE-BILD
+            //Wenn rechts und links einer ist
+            if(rightSib !== null && leftSib !== null){
+                //LINKES BILD DER REIHER
                 if(e.target.offsetTop === rightSib.offsetTop && e.target.offsetTop !== leftSib.offsetTop){
                     e.target.classList.add("thick")                
                     rightSib.classList.add("thin")
-                    rightSib.nextElementSibling.classList.add("thin")
+                    if(e.target.offsetTop == rightSib.nextElementSibling.offsetTop){
+                        rightSib.nextElementSibling.classList.add("thin")
+                    }
                 } 
-                if(e.target.offsetTop !== rightSib.offsetTop && e.target.offsetTop === leftSib.offsetTop){
+                //RECHTES BILD DER REIHE
+                else if(e.target.offsetTop !== rightSib.offsetTop && e.target.offsetTop === leftSib.offsetTop){
                     e.target.classList.add("thick")                
                     leftSib.classList.add("thin")
-                    leftSib.previousElementSibling.classList.add("thin")
-                } 
-                if(e.target.offsetTop === rightSib.offsetTop && e.target.offsetTop === leftSib.offsetTop){
+                    if(e.target.offsetTop == leftSib.previousElementSibling.offsetTop){
+                        leftSib.previousElementSibling.classList.add("thin")
+                    }
+                }
+                //MITTLERES BILD DER REIHE
+                else if(e.target.offsetTop === rightSib.offsetTop && e.target.offsetTop === leftSib.offsetTop){
                     e.target.classList.add("thick")                
                     leftSib.classList.add("thin")
                     rightSib.classList.add("thin")
                 } 
-            }       
-            
+                  
+            } 
         }
     });
     bilder[i].addEventListener("mouseleave", (e) => {
@@ -48,3 +95,4 @@ for(let i = 0;i < bilder.length;i++){
         }
     });
 }
+});
